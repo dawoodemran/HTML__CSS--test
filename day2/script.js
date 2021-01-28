faeb623ca86bbffcda9bbc2d714983af71ef1e86
DawoodEmran <rockdawoodde@gmail.com>
2021-01-28 09:25:56 +0430

    Added new js code

--- day2/script.js
+++ day2/script.js
@@ -1,13 +1,36 @@
-const panels = document.querySelectorAll('.panel')
-panels.forEach((panel) => {
-  panel.addEventListener('click', () => {
-    removeActiveClasses()
-    panel.classList.add('active')
-  })
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+  currentActive++
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length
+  }
+
+  update()
 })
 
-function removeActiveClasses() {
-  panels.forEach((panel) => {
-    panel.classList.remove('active')
+prev.addEventListener('click', () => {
+  currentActive--
+
+  if (currentActive < 1) {
+    currentActive = 1
+  }
+
+  update()
+})
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
   })
 }
